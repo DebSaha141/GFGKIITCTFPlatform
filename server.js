3f44c50ce8b308da20e5216c9470a9c3c4540cf7
DebSaha141 <debsaha141@gmail.com>
2025-05-07 02:00:29 +0530

    Ready for Deployment 2

--- server.js
+++ server.js
@@ -102,10 +102,10 @@ if (process.env.NODE_ENV === 'production') {
     app.set('trust proxy', 1);
 }
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 // Export for Vercel
 module.exports = app; 
