7d80e6ad6683526751ce17cd18fbbfa3a1011261
DebSaha141 <debsaha141@gmail.com>
2025-05-07 02:42:50 +0530

    More Changes

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ const app = express();
 global.ctfStatus = 'stopped';
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
